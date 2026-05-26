dbdec2abc34d97ecad0521e23321b9dcc1f1a53d
Mohammedanwarsha1 <mohammedanwarsha2019@gmail.com>
2026-05-26 16:05:46 +0530

    remaing of createProductValid

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import categoryRouter from "./category/category-router";
 import cookieParser from "cookie-parser";
+import productRouter from "./product/product-router";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/categories", categoryRouter);
+app.use("/product", productRouter);
 
 app.use(globalErrorHandler);
 
