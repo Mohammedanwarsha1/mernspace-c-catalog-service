c61b245e33e5296e31a97d240b8cf88cc83be8d0
Mohammedanwarsha1 <mohammedanwarsha2019@gmail.com>
2026-05-30 11:46:11 +0530

    add tenant access check

--- src/product/product-controller.ts
+++ src/product/product-controller.ts
@@ -7,6 +7,8 @@ import { Product } from "./product-types";
 import { FileStorage } from "../common/types/storage";
 import { UploadedFile } from "express-fileupload";
 import { v4 as uuidv4 } from "uuid";
+import { AuthRequest } from "../common/types";
+import { Roles } from "../common/constants";
 
 export class ProductController {
     constructor(
@@ -64,6 +66,26 @@ export class ProductController {
 
         const { productId } = req.params;
 
+        if ((req as AuthRequest).auth.role === Roles.MANAGER) {
+            const product = await this.productService.getProduct(
+                productId as string,
+            );
+            if (!product) {
+                return next(createHttpError(404, "Product not found"));
+            }
+
+            const tenant = (req as AuthRequest).auth?.tenant;
+
+            if (product.tenantId != String(tenant)) {
+                return next(
+                    createHttpError(
+                        403,
+                        "You are not allowed to access this product",
+                    ),
+                );
+            }
+        }
+
         let imageName: string | undefined;
         let oldImage: string | undefined;
 
@@ -96,7 +118,7 @@ export class ProductController {
             isPublish,
         } = req.body;
 
-        const product = {
+        const productToUpdate = {
             name,
             description,
             priceConfiguration: JSON.parse(priceConfiguration as string),
@@ -107,7 +129,10 @@ export class ProductController {
             image: imageName ? imageName : (oldImage as string),
         };
 
-        await this.productService.updateProduct(productId as string, product);
+        await this.productService.updateProduct(
+            productId as string,
+            productToUpdate,
+        );
 
         res.json({ id: productId });
     };
--- src/product/product-service.ts
+++ src/product/product-service.ts
@@ -20,4 +20,7 @@ export class ProductService {
             },
         );
     }
+    async getProduct(productId: string): Promise<Product | null> {
+        return await productModel.findOne({ _id: productId });
+    }
 }
